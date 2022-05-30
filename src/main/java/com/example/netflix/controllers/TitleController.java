181c78bbf11a0858d43bf1fa94dec5c1c07965e8
Joel Hurtado <joel29102000@gmail.com>
2022-05-30 16:29:31 +0200

    Creacion del controller

--- src/main/java/com/example/netflix/controllers/TitleController.java
+++ src/main/java/com/example/netflix/controllers/TitleController.java
@@ -1,4 +1,20 @@
 package com.example.netflix.controllers;
 
+import com.example.netflix.repositories.TitleRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TitleController {
+    private final TitleRepository repository;
+    TitleController(TitleRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/best")
+    Object best(@RequestParam(defaultValue =  "10") Integer top) {
+        return repository.findTopRating(PageRequest.of(0,top));
+    }
 }
--- src/main/java/com/example/netflix/repositories/TitleRepository.java
+++ src/main/java/com/example/netflix/repositories/TitleRepository.java
@@ -1,10 +1,16 @@
 package com.example.netflix.repositories;
 
 import com.example.netflix.models.Title;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "titles", path = "titles")
 public interface TitleRepository extends PagingAndSortingRepository<Title, Long> {
-
+    @Query(value = "SELECT * FROM title  t ORDER BY t.user_rating DESC", nativeQuery = true)
+    List<Title> findTopRating(PageRequest pageRequest);
 }
